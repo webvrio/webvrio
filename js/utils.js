411673126c70367df8a366135ac4029276bc19a7
Michael Blix <mkeblx@gmail.com>
2016-04-05 22:42:27 -0400

    misc

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+// utils.js
+
+var VRUtil = {
+
+  isVRBrowser: function() {
+    var UA = window.navigator.userAgent;
+    // current only one VR browser
+    // Samsung Internet which contains 'VR' in UA
+    return UA.indexOf('VR') !== -1;
+  },
+
+  isWebVrApiPresent: function() {
+    return !! window.navigator.getVRDevices || !! window.navigator.getVRDisplays;
+  }
+
+};
